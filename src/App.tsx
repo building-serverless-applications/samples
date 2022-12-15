103aced475a6529a05baa1411363364ecc4cc4bb
Evan Anderson <evan.k.anderson@gmail.com>
2022-12-15 13:44:58 -0800

    Update UI to load (static) data from endpoint.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
 import './App.css';
-import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Container, CssBaseline, Grid, ThemeProvider } from '@mui/material'
-import theme from './theme'
+import { Container, CssBaseline, ThemeProvider } from '@mui/material';
+import theme from './theme';
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline enableColorScheme />
       <Container maxWidth={false}>
-        <Grid container justifyContent='center' alignItems='center' spacing={2}>
-          <Grid item xs={6}><Card>
-            <CardActionArea>
-            <CardMedia component='img' alt='Knative logo' image='https://knative.dev/docs/images/logo/rgb/knative-logo-rgb.png'></CardMedia>
-            <CardContent><h2>First item</h2>
-            
-            Some info
-            </CardContent>
-            </CardActionArea>
-            <CardActions><Button size='small'>Get Info</Button></CardActions>
-            </Card></Grid>
-        </Grid>
+        <Dashboard url='/my/dashboard'></Dashboard>
       </Container>
     </ThemeProvider>
   );
