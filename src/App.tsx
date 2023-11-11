1b4d76d432485b7149a4802e412e154793d245e9
Evan Anderson <evan@stacklok.com>
2023-11-10 16:36:27 -0800

    Convert to MUI, add theme, single card UX

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Button, Card, CardActionArea, CardActions, CardContent,
+  CardMedia, Container, CssBaseline, Grid, ThemeProvider, createTheme} from "@mui/material";
 
 function App() {
+  const imgUrl = 'https://knative.dev/docs/images/logo/rgb/knative-logo-rgb.png';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={createTheme({})}>
+      <CssBaseline enableColorScheme />
+      <Container maxWidth={false}>
+        <Grid container justifyContent='center' alignItems='center' spacing={2}>
+          <Grid item xs={6}><Card>
+            <CardActionArea>
+              <CardMedia component='img' alt='Knative logo' image={imgUrl}>
+              </CardMedia>
+              <CardContent><h2>First item</h2>
+                Some info
+              </CardContent>
+            </CardActionArea>
+            <CardActions><Button size='small'>Get Info</Button></CardActions>
+          </Card></Grid>
+        </Grid>
+      </Container>
+    </ThemeProvider>
   );
 }
 
